89426e17c1582fb11d4012d53aeeab3789acb52e
thynkon <thynkon@protonmail.com>
2022-05-20 08:52:17 +0200

    Use class instead of functions to render components

--- components/AuthenticationForm.tsx
+++ components/AuthenticationForm.tsx
@@ -1,77 +1,103 @@
 import { Text, TextInput, StyleSheet, View, Pressable } from "react-native";
-import { useState } from "react"
+import { Component } from "react"
 import { config } from "../config"
 import axios from 'axios'
 
-export function AuthenticationForm() {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-
-    return (
-        <View style={styles.form}>
-            <Text style={styles.title}>Nextep</Text>
-            <TextInput placeholder="Email" style={styles.input} onChangeText={(newUsername) => setUsername(newUsername)}/>
-            <TextInput placeholder="Password" secureTextEntry={true} style={styles.input} onChangeText={(newPassword) => setPassword(newPassword)}/>
-            <Pressable style={styles.button} onPress={() => okPressed(username, password)}>
-                <Text style={styles.text}>Login</Text>
-            </Pressable>
-        </View>
-    );
-}
-
-function okPressed(username: string, password: string) {
-    axios.post(config.api_url + "/me/token", {
-        'username': username,
-        'password': password
-    }).then(response => {
-        console.log(response.data);
-    }).catch(err => {
-        console.log(err);
-    });
-}
-
-const DIMENSIONS = {
-    margin: 12,
-    height: 35,
-    fontSize: 15,
+export type Props = {
+    username: string,
+    password: string,
+    message: string,
 };
 
-
-const styles = StyleSheet.create({
-    title: {
-        textAlign: 'center',
-        fontWeight: 'bold',
-        fontStyle: 'italic',
-        fontSize: DIMENSIONS.fontSize * 2,
-    },
-    input: {
-        height: DIMENSIONS.height,
-        margin: DIMENSIONS.margin,
-        borderWidth: 1,
-        borderRadius: 3,
-        padding: 10,
-        fontSize: DIMENSIONS.fontSize,
-        backgroundColor: '#fff',
-    },
-    label: {
-        fontSize: DIMENSIONS.fontSize,
-        margin: DIMENSIONS.margin,
-    },
-    button: {
-        margin: DIMENSIONS.margin,
-        height: DIMENSIONS.height,
-        alignItems: 'center',
-        justifyContent: 'center',
-        fontWeight: 'bold',
-        backgroundColor: '#000',
-    },
-    form: {
-        width: '100%',
-        padding: 10,
-    },
-    text: {
-        color: 'white',
-        textTransform: 'uppercase',
-        fontWeight: 'bold',
+export class AuthenticationForm extends Component {
+    state: {
+        username: string,
+        password: string,
+        message: string,
     }
-});
+
+    DIMENSIONS = {
+        margin: 12,
+        height: 35,
+        fontSize: 15,
+    };
+
+
+    readonly styles = StyleSheet.create({
+        title: {
+            textAlign: 'center',
+            fontWeight: 'bold',
+            fontStyle: 'italic',
+            fontSize: this.DIMENSIONS.fontSize * 2,
+        },
+        input: {
+            height: this.DIMENSIONS.height,
+            margin: this.DIMENSIONS.margin,
+            borderWidth: 1,
+            borderRadius: 3,
+            padding: 10,
+            fontSize: this.DIMENSIONS.fontSize,
+            backgroundColor: '#fff',
+        },
+        label: {
+            fontSize: this.DIMENSIONS.fontSize,
+            margin: this.DIMENSIONS.margin,
+        },
+        button: {
+            margin: this.DIMENSIONS.margin,
+            height: this.DIMENSIONS.height,
+            alignItems: 'center',
+            justifyContent: 'center',
+            fontWeight: 'bold',
+            backgroundColor: '#000',
+        },
+        form: {
+            width: '100%',
+            padding: 10,
+        },
+        text: {
+            color: 'white',
+            textTransform: 'uppercase',
+            fontWeight: 'bold',
+        },
+        message: {
+            height: this.DIMENSIONS.height,
+        }
+    });
+
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+            message: '',
+        }
+
+    }
+    okPressed(username: string, password: string) {
+        axios.post(config.api_url + "/me/token", {
+            'username': username,
+            'password': password
+        }).then(response => {
+            this.setState({message: 'User successfully authenticated!'});
+        }).catch(err => {
+            if (err.response.status === 401) {
+                this.setState({message: 'Authentication failed! Please check your credentials and try again.'});
+            }
+        });
+    }
+
+    render() {
+        return (
+            <View style={this.styles.form}>
+                <Text style={this.styles.title}>Nextep</Text>
+                <Text style={[this.styles.error, this.styles.label]}>{this.state.message}</Text>
+                <TextInput placeholder="Email" style={this.styles.input} onChangeText={(newUsername) => this.setState({ username: newUsername })} />
+                <TextInput placeholder="Password" secureTextEntry={true} style={this.styles.input} onChangeText={(newPassword) => this.setState({ password: newPassword })} />
+                <Pressable style={this.styles.button} onPress={() => this.okPressed(this.state.username, this.state.password)}>
+                    <Text style={this.styles.text}>Login</Text>
+                </Pressable>
+            </View>
+        );
+    }
+}
