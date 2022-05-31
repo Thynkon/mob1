8c913d243baef549ac124e3d1e29d403bf33160a
thynkon <thynkon@protonmail.com>
2022-05-31 21:36:09 +0200

    Rewrite MainContainer using function syntax

--- navigation/MainContainer.tsx
+++ navigation/MainContainer.tsx
@@ -15,42 +15,41 @@ const profileName = "Profile";
 
 const Tab = createBottomTabNavigator();
 
-export default class MainContainer extends Component {
-  render() {
-    return (
-      // <NavigationContainer>
-        <Tab.Navigator
-          initialRouteName={newsName}
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-              let iconName;
-              let rn = route.name;
+export default function MainContainer() {
+  return (
+    // <NavigationContainer>
+    <Tab.Navigator
+      initialRouteName={newsName}
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          let rn = route.name;
 
-              if (rn === newsName) {
-                iconName = focused ? 'newspaper' : 'newspaper-outline';
-              } else if (rn === walletName) {
-                iconName = focused ? 'wallet' : 'wallet-outline';
-              } else if (rn === profileName) {
-                iconName = focused ? 'person' : 'person-outline';
-              }
+          if (rn === newsName) {
+            iconName = focused ? 'newspaper' : 'newspaper-outline';
+          } else if (rn === walletName) {
+            iconName = focused ? 'wallet' : 'wallet-outline';
+          } else if (rn === profileName) {
+            iconName = focused ? 'person' : 'person-outline';
+          }
 
-              // You can return any component that you like here!
-              return <Ionicons name={iconName} size={size} color={color} />;
-            },
-          })}
-          tabBarOptions={{
-            activeTintColor: 'tomato',
-            inactiveTintColor: 'grey',
-            labelStyle: { paddingBottom: 10, fontSize: 10 },
-            style: { padding: 10, height: 70 }
-          }}>
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'grey',
+        labelStyle: { paddingBottom: 10, fontSize: 10 },
+        style: { padding: 10, height: 70 }
+      }}>
 
-          <Tab.Screen name={newsName} component={NewsScreen} />
-          <Tab.Screen name={walletName} component={WalletScreen} />
-          <Tab.Screen name={profileName} component={ProfileScreen} />
+      <Tab.Screen name={newsName} component={NewsScreen} />
+      <Tab.Screen name={walletName} component={WalletScreen} />
+      <Tab.Screen name={profileName} component={ProfileScreen} />
 
-        </Tab.Navigator>
-      // </NavigationContainer>
-    );
-  }
+    </Tab.Navigator>
+    // </NavigationContainer>
+  );
 }
