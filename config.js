ea011a44afaf6c438dcd7746a55030c3f812f96d
thynkon <thynkon@protonmail.com>
2022-06-17 13:17:42 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const config = {
-    // api_url: 'https://mockapi.mycpnv.ch/api/nxp',
-    // img_url: 'https://mockapi.mycpnv.ch/storage/pics'
-    api_url: 'http://192.168.161.33:8001/api/nxp',
-    img_url: 'http://192.168.161.33:8001/storage/pics',
+    api_url: 'https://mockapi.mycpnv.ch/api/nxp',
+    img_url: 'https://mockapi.mycpnv.ch/storage/pics'
+    // api_url: 'http://192.168.161.33:8001/api/nxp',
+    // img_url: 'http://192.168.161.33:8001/storage/pics',
 };
