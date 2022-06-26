980b11761736402a33b408e6a4bc599eb2275370
thynkon <thynkon@protonmail.com>
2022-06-26 11:36:10 +0200

    Point api to heroku instance

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
+const host = 'https://mob1-api.herokuapp.com'
+
 export const config = {
-    // api_url: 'https://mockapi.mycpnv.ch/api/nxp',
-    // img_url: 'https://mockapi.mycpnv.ch/storage/pics'
-    api_url: 'http://192.168.161.33:8001/api/nxp',
-    img_url: 'http://192.168.161.33:8001/storage/pics',
+    api_url: `${host}/api/nxp`,
+    img_url: `${host}/storage/pics`,
 };
